88a97e043a1c959566602577e6e9f3a77f827ef0
azis07 <abd.azispc@gmail.com>
2020-10-20 20:40:24 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-./PhoenixMiner -pool eth-eu1.nanopool.org:9999 -pool2 eth-eu2.nanopool.org:8888 -wal 0x207ab8609b1ebba83cd98837c5e10194f456d8d2.rig1/apam@xxx.com -nvidia 1
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+./PhoenixMiner -pool eth-eu1.nanopool.org:9999 -pool2 eth-eu2.nanopool.org:8888 -wal 0x207ab8609b1ebba83cd98837c5e10194f456d8d2.rig1/apam@xxx.com -nvidia -1
