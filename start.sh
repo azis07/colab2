63a9f3ecea60d0adc7c7292a1cab97687ff72e5b
azis07 <abd.azispc@gmail.com>
2020-10-20 16:26:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./PhoenixMiner -pool -nvidia eth-eu1.nanopool.org:9999 -pool2 eth-eu2.nanopool.org:8888 -wal YOUR_WALLET.YOUR_WORKER/YOUR_EMAIL
+./PhoenixMiner -pool eth-eu1.nanopool.org:9999 -pool2 eth-eu2.nanopool.org:8888 -wal  YOUR_WALLET.YOUR_WORKER/YOUR_EMAIL -nvidia
